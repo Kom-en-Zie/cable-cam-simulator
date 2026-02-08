6bfce75d972fc5c5c7da59156191d04b0a6ed8c8
Rens Pols <rens@cepols.nl>
2026-02-08 20:29:05 +0100

    Added TimeState

---
+++ src/nl/komenzie/cableCam/time/TimeState.kt
@@ -0,0 +1,12 @@
+package nl.komenzie.cableCam.time
+
+import kotlin.time.Duration
+
+class TimeState(
+    val timePassed: Duration = Duration.ZERO,
+) {
+  fun update(deltaTime: Duration): TimeState {
+    return TimeState(timePassed + deltaTime)
+  }
+
+}
