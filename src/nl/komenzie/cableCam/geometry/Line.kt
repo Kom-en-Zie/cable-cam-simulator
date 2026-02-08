7c369baa4209b291608ce186ed5dd1d6f20c57f5
Rens Pols <rens@cepols.nl>
2026-02-08 20:28:48 +0100

    Added Line and Point classes

---
+++ src/nl/komenzie/cableCam/geometry/Line.kt
@@ -0,0 +1,8 @@
+package nl.komenzie.cableCam.geometry
+
+import kotlin.math.pow
+import kotlin.math.sqrt
+
+data class Line(val p1: Point, val p2: Point) {
+  val length: Double get() = sqrt((p1.x - p2.x).pow(2) + (p1.y - p2.y).pow(2))
+}
---
+++ src/nl/komenzie/cableCam/geometry/Point.kt
@@ -0,0 +1,3 @@
+package nl.komenzie.cableCam.geometry
+
+data class Point(val x: Double, val y: Double)
