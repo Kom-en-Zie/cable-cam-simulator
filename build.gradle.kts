7b6be0837e0359d95992ae640b70864d1df21618
Rens Pols <rens@cepols.nl>
2026-02-11 22:37:28 +0100

    Added Ktor server and serialization dependencies

--- build.gradle.kts
+++ build.gradle.kts
@@ -10,6 +10,13 @@ repositories {
 }
 
 dependencies {
+    // Ktor Server and Engine
+    implementation("io.ktor:ktor-server-netty:3.4.0")
+    implementation("io.ktor:ktor-server-websockets:3.4.0")
+
+    // Serialization
+    implementation("io.ktor:ktor-server-content-negotiation:3.4.0")
+    implementation("io.ktor:ktor-serialization-kotlinx-json:3.4.0")
     testImplementation(kotlin("test"))
 }
 
