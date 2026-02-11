fbfab3bad87373b0e3053c3c1a3902bd983a8062
Rens Pols <rens@cepols.nl>
2026-02-11 23:21:19 +0100

    Added @Serializable annotation to geometry, motor, and time classes

--- src/main/kotlin/parts/motors/MotorState.kt
+++ src/main/kotlin/parts/motors/MotorState.kt
@@ -1,5 +1,6 @@
 package nl.komenzie.cableCam.parts.motors
 
+import kotlinx.serialization.Serializable
 import nl.komenzie.cableCam.util.time.toSeconds
 import kotlin.time.Duration
 
@@ -7,6 +8,7 @@ import kotlin.time.Duration
  * @param speed The Current speed (m/s) of cable input/output. Pulling cable in is a negative number
  * @param properties The properties of this motor
  */
+@Serializable
 class MotorState(
     val properties: MotorProperties,
     var speed: Double,
