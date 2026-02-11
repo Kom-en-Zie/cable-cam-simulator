3860c0429e2bfab038d8c96b5e37b8f78c2245b5
Rens Pols <rens@cepols.nl>
2026-02-11 20:56:52 +0100

    Optimized Line length calculation

--- src/nl/komenzie/cableCam/geometry/Line.kt
+++ src/nl/komenzie/cableCam/geometry/Line.kt
@@ -4,5 +4,5 @@ import kotlin.math.pow
 import kotlin.math.sqrt
 
 data class Line(val p1: Point, val p2: Point) {
-    val length: Double get() = sqrt((p1.x - p2.x).pow(2) + (p1.y - p2.y).pow(2))
+    val length: Double = sqrt((p1.x - p2.x).pow(2) + (p1.y - p2.y).pow(2))
 }
