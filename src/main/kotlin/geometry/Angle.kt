54604f9bbc92fa90d1d40476d1c477e1de87b12b
Rens Pols <rens@cepols.nl>
2026-03-02 21:09:41 +0100

    Refactor: Update `Angle` class to use radians as the primary unit

--- src/main/kotlin/geometry/Angle.kt
+++ src/main/kotlin/geometry/Angle.kt
@@ -1,9 +1,9 @@
 package nl.komenzie.cableCam.geometry
 
 import kotlinx.serialization.Serializable
-import java.lang.Math.toRadians
+import java.lang.Math.toDegrees
 
 @Serializable
-data class Angle(val degrees: Double) {
-    val radians: Double = toRadians(degrees)
+data class Angle(val radians: Double) {
+    val degrees: Double = toDegrees(radians)
 }
