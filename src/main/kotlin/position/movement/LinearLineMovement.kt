e237caf8644463153b8aef01315eb6c0980b5f26
Rens Pols <rens@cepols.nl>
2026-03-02 22:13:44 +0100

    Refactor: Remove unused `initialMovement` parameter from `Movement` and `LinearLineMovement` constructors

--- src/main/kotlin/position/movement/LinearLineMovement.kt
+++ src/main/kotlin/position/movement/LinearLineMovement.kt
@@ -1,17 +1,15 @@
 package nl.komenzie.cableCam.position.movement
 
 import nl.komenzie.cableCam.geometry.Point
-import nl.komenzie.cableCam.movementVector.MovementVector
 import kotlin.time.Duration
 
 class LinearLineMovement(
     cPosStart: Point,
     cPosEnd: Point,
-    initialMovement: MovementVector,
     startTime: Duration,
     speed: Double,
     acceleration: Double,
-) : Movement(cPosStart, cPosEnd, initialMovement, startTime, speed, acceleration) {
+) : Movement(cPosStart, cPosEnd, startTime, speed, acceleration) {
 
     override fun calculateDesiredPositionRelative(relativeTime: Duration): Point {
         TODO("Not yet implemented")
