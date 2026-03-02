e5ba7f2dd2e1f8d3bec4f62f4c22e64b848e7665
Rens Pols <rens@cepols.nl>
2026-03-02 19:55:39 +0100

    Add base `Movement` abstract class and initialised `LinearLineMovement` implementation for position handling

---
+++ src/main/kotlin/position/movement/Movement.kt
@@ -0,0 +1,23 @@
+package nl.komenzie.cableCam.position.movement
+
+import nl.komenzie.cableCam.geometry.Point
+import kotlin.time.Duration
+
+abstract class Movement(
+    val cPosStart: Point,
+    val cPosEnd: Point,
+    val startTime: Duration,
+    val speed: Double,
+    val acceleration: Double,
+) {
+
+    /**
+     * @param relativeTime The time progression that needs to be processed (currentTime - startTime)
+     * @return Point The point on which the cPos should be on that time instance
+     */
+    abstract fun changePropertiesRelative(relativeTime: Duration): Point
+
+    fun changeProperties(currentTime: Duration): Point {
+        return changePropertiesRelative(currentTime - startTime)
+    }
+}
