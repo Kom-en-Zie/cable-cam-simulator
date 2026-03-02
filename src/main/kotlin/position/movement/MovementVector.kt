6148298162cf49032b9b3d521d6c8ebad1e40c6e
Rens Pols <rens@cepols.nl>
2026-03-02 20:22:11 +0100

    Add `MovementVector` class to calculate new position based on angle, speed, and time

---
+++ src/main/kotlin/position/movement/MovementVector.kt
@@ -0,0 +1,24 @@
+package nl.komenzie.cableCam.position.movement
+
+import nl.komenzie.cableCam.geometry.Angle
+import nl.komenzie.cableCam.geometry.Point
+import nl.komenzie.cableCam.util.time.toSeconds
+import kotlin.math.cos
+import kotlin.math.sin
+import kotlin.time.Duration
+
+/**
+ * @param angle The angle of the movement vector in degrees
+ * @param speed The speed in m/s
+ */
+class MovementVector(
+    val angle: Angle,
+    val speed: Double,
+) {
+    fun newPos(startPos: Point, time: Duration): Point {
+        val distance = speed * time.toSeconds()
+        val dx = distance * cos(angle.radians)
+        val dy = distance * sin(angle.radians)
+        return Point(startPos.x + dx, startPos.y + dy)
+    }
+}
