d54468773889ca4712eb58795566e1a7afffdc2b
Rens Pols <rens@cepols.nl>
2026-03-02 21:11:27 +0100

    Add `angle` property to `Line` class to compute angle using `atan2`

--- src/main/kotlin/geometry/Line.kt
+++ src/main/kotlin/geometry/Line.kt
@@ -1,10 +1,12 @@
 package nl.komenzie.cableCam.geometry
 
 import kotlinx.serialization.Serializable
+import kotlin.math.atan2
 import kotlin.math.pow
 import kotlin.math.sqrt
 
 @Serializable
 data class Line(val p1: Point, val p2: Point) {
     val length: Double = sqrt((p1.x - p2.x).pow(2) + (p1.y - p2.y).pow(2))
+    val angle: Angle = Angle(atan2(p2.y - p1.y, p2.x - p1.x))
 }
