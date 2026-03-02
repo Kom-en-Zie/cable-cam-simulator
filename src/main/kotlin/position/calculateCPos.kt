ac8b0b0a5a9970adf204de824e2192e3cf0b141a
Rens Pols <rens@cepols.nl>
2026-03-02 20:54:16 +0100

    Make `calculateCPos` accept optional `t1` and `t2` arguments and update rope length calculations accordingly

--- src/main/kotlin/position/calculateCPos.kt
+++ src/main/kotlin/position/calculateCPos.kt
@@ -16,25 +16,29 @@ import kotlin.math.sqrt
  * and aPos with radius lengthL2
  * that has the lowest y coordinate.
  */
-fun CableCamState.calculateCPos(): Point {
+fun CableCamState.calculateCPos(t1: Double? = null, t2: Double? = null): Point {
+    val t1Local = t1 ?: this.t1
+    val t2Local = t2 ?: this.t2
+    val l1Local = if (t1 != null || t2 != null) this.calculateL1(t1Local, t2Local) else this.lengthL1
+    val l2Local = if (t1 != null || t2 != null) this.calculateL2(t1Local, t2Local) else this.lengthL2
 
     /** [d] Distance between oPos and aPos */
     val d: Double = Line(oPos, aPos).length
 
-    if (d > (t1 + t2) / 4.0) throw InvalidCableCamStateException("The ropes are shorter than the minimum length required for oPos and aPos")
-    if (d > lengthL1 + lengthL2) throw InvalidCableCamStateException(
+    if (d > (t1Local + t2Local) / 4.0) throw InvalidCableCamStateException("The ropes are shorter than the minimum length required for oPos and aPos")
+    if (d > l1Local + l2Local) throw InvalidCableCamStateException(
         "The circles of possibilities do not intersect." +
                 " IMPORTANT: This is a math or code error, since the 'd > (t1 + t2) / 4.0' check is already passed"
     )
-    if (d < abs(lengthL1 - lengthL2)) throw InvalidCableCamStateException(
+    if (d < abs(l1Local - l2Local)) throw InvalidCableCamStateException(
         "The circles of possibilities do not intersect. (one circle is within the other one)"
     )
 
     /** [a] The distance from the first center to the projection of the intersection points on the line connecting the centers */
-    val a: Double = (lengthL1.pow(2) - lengthL2.pow(2) + d.pow(2)) / (2 * d)
+    val a: Double = (l1Local.pow(2) - l2Local.pow(2) + d.pow(2)) / (2 * d)
 
     /** [h] The distance from that line to the intersection points */
-    val h: Double = sqrt(lengthL1.pow(2) - a.pow(2))
+    val h: Double = sqrt(l1Local.pow(2) - a.pow(2))
 
     // 1. Find the point P2 (the projection point on the line between centers)
     val x2 = oPos.x + a * (aPos.x - oPos.x) / d
