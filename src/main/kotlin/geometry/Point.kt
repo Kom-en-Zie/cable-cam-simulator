0e0ac7eea1eebcc001dc2612a0ebb7b346c86766
Rens Pols <rens@cepols.nl>
2026-03-05 20:20:18 +0100

    Add `plus` and `minus` operator functions to `Point` class

--- src/main/kotlin/geometry/Point.kt
+++ src/main/kotlin/geometry/Point.kt
@@ -3,4 +3,7 @@ package nl.komenzie.cableCam.geometry
 import kotlinx.serialization.Serializable
 
 @Serializable
-data class Point(val x: Double, val y: Double)
+data class Point(val x: Double, val y: Double) {
+    operator fun plus(other: Point) = Point(x + other.x, y + other.y)
+    operator fun minus(other: Point) = Point(x - other.x, y - other.y)
+}
