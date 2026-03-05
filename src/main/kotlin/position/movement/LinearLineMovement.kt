07694df530c0d64f1365860d2f52361619f4cc2a
Rens Pols <rens@cepols.nl>
2026-03-05 20:22:35 +0100

    Add distance calculation for top-speed and deceleration phases in `LinearLineMovement`

--- src/main/kotlin/position/movement/LinearLineMovement.kt
+++ src/main/kotlin/position/movement/LinearLineMovement.kt
@@ -1,9 +1,12 @@
 package nl.komenzie.cableCam.position.movement
 
 import nl.komenzie.cableCam.cartState.CartState
+import nl.komenzie.cableCam.geometry.Line
 import nl.komenzie.cableCam.geometry.Point
+import nl.komenzie.cableCam.movementVector.MovementVector
 import nl.komenzie.cableCam.util.time.toSeconds
 import kotlin.math.pow
+import kotlin.math.sqrt
 import kotlin.time.Duration
 
 class LinearLineMovement(
@@ -15,16 +18,40 @@ class LinearLineMovement(
 ) : Movement(cPosStart, cPosEnd, startTime, speed, acceleration) {
     val accelerationDistance = speed.pow(2) / (2 * acceleration)
     val accelerationTime = speed / acceleration
+    val track = Line(cPosStart, cPosEnd)
+    val trackLength = track.length
+    val topSpeedTravelingDistance = trackLength - 2 * accelerationDistance
+    val topSpeedTravelingTime = topSpeedTravelingDistance / speed
 
     override fun calculateDesiredCartState(relativeTime: Duration): CartState {
-        TODO("Not yet implemented")
+        val desiredPos = cPosStart + calculateRelativePos(relativeTime)
+        val desiredMovementVector = MovementVector(track.angle, TODO("Speed at this relativeTime"))
+        return CartState(desiredPos, desiredMovementVector)
+    }
+
+    private fun calculateRelativePos(relativeTime: Duration): Point {
+        // c = sqrt (a^2 + b^2) with a=b -> c = sqrt(2) * a -> a = c / sqrt(2)
+        val traveledXandY = calculateDistanceToStart(relativeTime) / sqrt(2.0)
+        return Point(traveledXandY, traveledXandY)
     }
 
     private fun calculateDistanceToStart(relativeTime: Duration): Double {
         val time = relativeTime.toSeconds()
-        if (time > accelerationTime) {
-            return speed * (time - accelerationTime) + accelerationDistance
-        }
-        TODO("Distance when still accelerating")
+        // Still accelerating
+        if (time < accelerationTime) return calculateAcceleratingDistance(time)
+
+        // Traveling at top speed (before required deceleration)
+        val traveledDistance = speed * (time - accelerationTime) + accelerationDistance
+        if (traveledDistance <= trackLength - accelerationDistance) return traveledDistance
+
+        // Currently decelerating
+        val deceleratingTime = time - accelerationTime - topSpeedTravelingTime
+        val deceleratingDistance =
+            accelerationDistance - calculateAcceleratingDistance(accelerationTime - deceleratingTime)
+        return accelerationDistance + topSpeedTravelingDistance + deceleratingDistance
+    }
+
+    private fun calculateAcceleratingDistance(relativeTime: Double): Double {
+        return 0.5 * acceleration * relativeTime.pow(2)
     }
 }
