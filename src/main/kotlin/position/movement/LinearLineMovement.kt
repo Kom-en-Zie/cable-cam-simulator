8a12a0932b9e8904d99bbc765de26c060c5900cb
Rens Pols <rens@cepols.nl>
2026-03-05 18:50:25 +0100

    Add distance and time calculations for acceleration phase in `LinearLineMovement`

--- src/main/kotlin/position/movement/LinearLineMovement.kt
+++ src/main/kotlin/position/movement/LinearLineMovement.kt
@@ -2,6 +2,8 @@ package nl.komenzie.cableCam.position.movement
 
 import nl.komenzie.cableCam.cartState.CartState
 import nl.komenzie.cableCam.geometry.Point
+import nl.komenzie.cableCam.util.time.toSeconds
+import kotlin.math.pow
 import kotlin.time.Duration
 
 class LinearLineMovement(
@@ -11,8 +13,18 @@ class LinearLineMovement(
     speed: Double,
     acceleration: Double,
 ) : Movement(cPosStart, cPosEnd, startTime, speed, acceleration) {
+    val accelerationDistance = speed.pow(2) / (2 * acceleration)
+    val accelerationTime = speed / acceleration
 
     override fun calculateDesiredCartState(relativeTime: Duration): CartState {
         TODO("Not yet implemented")
+    }
+
+    private fun calculateDistanceToStart(relativeTime: Duration): Double {
+        val time = relativeTime.toSeconds()
+        if (time > accelerationTime) {
+            return speed * (time - accelerationTime) + accelerationDistance
+        }
+        TODO("Distance when still accelerating")
     }
 }
