4e62bcd12a9a4316fbf8ecf8e12a9960a3353c52
Rens Pols <rens@cepols.nl>
2026-03-07 16:08:37 +0100

    Add `sanitiseMovementQueue` method to filter invalid movements in `MovementQueue`

---
+++ src/main/kotlin/position/movement/queue/sanitiseMovementQueue.kt
@@ -0,0 +1,18 @@
+package nl.komenzie.cableCam.position.movement.queue
+
+import kotlin.time.Duration
+
+fun MovementQueue.sanitiseMovementQueue() {
+    val movementArray = this.toArray()
+    var currentEndTime: Duration? = null
+    movementArray.forEach { m ->
+        if (this.cableCamState.timeState.timePassed < m.startTime + m.endTime) {
+            return@forEach this.removeElement(m)
+        }
+        if (currentEndTime != null && m.startTime < currentEndTime) {
+            return@forEach this.removeElement(m)
+        }
+        // TODO: check if the distance between the end and startpoint can be reached within time
+        currentEndTime = m.endTime
+    }
+}
