cf3cae52763a1e5e417f775df552b69c7964c6ab
Rens Pols <rens@cepols.nl>
2026-03-07 10:54:39 +0100

    Add `MovementQueue` to manage movement commands in `CableCamState`

---
+++ src/main/kotlin/position/movement/queue/MovementQueue.kt
@@ -0,0 +1,7 @@
+package nl.komenzie.cableCam.position.movement.queue
+
+import nl.komenzie.cableCam.position.movement.Movement
+
+class MovementQueue {
+    private val queue = ArrayDeque<Movement>()
+}
