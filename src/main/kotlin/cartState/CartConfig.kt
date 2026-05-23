12da50442ccac7d844957df4ed803061ba3b6c5f
Rens Pols <rens@cepols.nl>
2026-05-23 23:08:01 +0200

    Make `CartConfig` serializable by adding `@Serializable` annotation

--- src/main/kotlin/cartState/CartConfig.kt
+++ src/main/kotlin/cartState/CartConfig.kt
@@ -1,5 +1,8 @@
 package nl.komenzie.cableCam.cartState
 
+import kotlinx.serialization.Serializable
+
+@Serializable
 data class CartConfig(
     val maxSpeed: Double = 10.0,
     val acceleration: Double = 2.0,
